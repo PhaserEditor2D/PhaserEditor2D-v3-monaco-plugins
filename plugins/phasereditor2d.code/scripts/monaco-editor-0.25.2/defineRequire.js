a25cac8be2e25eff8238e79e3eae383f99e39cda
PhaserEditor2D <developers@phasereditor2d.com>
2023-07-18 15:52:05 -0400

    Uses absolute path in defineRequire.

--- plugins/phasereditor2d.code/scripts/monaco-editor-0.25.2/defineRequire.js
+++ plugins/phasereditor2d.code/scripts/monaco-editor-0.25.2/defineRequire.js
@@ -1,1 +1,1 @@
-var require = { paths: { 'vs': 'app/plugins/phasereditor2d.code/scripts/monaco-editor-0.25.2/min/vs' } };
+var require = { paths: { 'vs': '/editor/app/plugins/phasereditor2d.code/scripts/monaco-editor-0.25.2/min/vs' } };
